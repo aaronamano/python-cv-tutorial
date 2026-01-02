a6976ef57d3896d8d40a3456bc6287da1f40ccb6
aaronamano <aaroncedric2005@yahoo.com>
2026-01-02 12:25:52 -0500

    read, resize, rotate an image as well as display and save new image

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
+# read, resize, rotate an image
+# display and save new image
 import cv2
 
+# read image
 img = cv2.imread('assets/mash.jpg', 1)
-img = cv2.resize(img, (400,400))
-
 # -1, cv2.IMREAD_COLOR : color image
 # 0, cv2.IMREAD_GRAYSCALE : grayscale image
 # 1, cv2.IMREAD_UNCHANGED : image as such including alpha channel
 
+# resize image
+img = cv2.resize(img, (0, 0), fx=0.5, fy=0.5)
+
+# rotate image
+img = cv2.rotate(img, cv2.ROTATE_90_COUNTERCLOCKWISE)
+
+# creates and saves a new image
+cv2.imwrite('new_img.jpg', img)
+
+# displays image
 cv2.imshow('Image', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
